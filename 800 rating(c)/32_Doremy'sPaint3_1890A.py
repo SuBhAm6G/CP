b2daeb9c972a47ade05ab0adf41d11b1c47f9e22
Subham6G <subhamdharoff@gmail.com>
2025-11-09 20:39:33 +0530

    Update 32_Doremy'sPaint3_1890A.py
    update 32

--- 800 rating(c)/32_Doremy'sPaint3_1890A.py
+++ 800 rating(c)/32_Doremy'sPaint3_1890A.py
@@ -1,15 +1,15 @@
 t=int(input())
 for _ in range(t):
     n=int(input())
-    arr=[]
-    arr=list(map(int, input().split()))
-    s=set(arr)
+    ar=[]
+    ar=list(map(int, input().split()))
+    s=set(ar)
     if(len(s)==1):
         print('YES')
     elif(len(s)==2):
         l=list(s)
-        count1=arr.count(l[0])
-        count2=arr.count(l[1])
+        count1=ar.count(l[0])
+        count2=ar.count(l[1])
         if(abs(count1-count2)<=1):
             print('YES')
         else:

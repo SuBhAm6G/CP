d28040185b0482dca0beafd1359eb5229c28ffdc
Subham6G <subhamdharoff@gmail.com>
2025-12-22 11:57:36 +0530

    22 12 25

---
+++ 800 rating(c)/49_1806AWalking Master.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    moves=0
+    a,b,c,d=map(int,input().split())
+    #y-coordinate equating
+    moves+=(d-b)
+    if (moves<0): 
+        print(-1)
+        continue
+    if c<=(a+moves):
+        moves+=(a+moves)-c
+        print(moves)
+    else: print(-1)
